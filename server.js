82920251c40e3baf3195abf73a212ee48f2f7a07
ElSauron-dot <nooborproefe@gmail.com>
2025-08-18 13:36:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,55 @@
-const WebSocket = require('ws');
-const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
-const wss = new WebSocket.Server({ port: 8080 });
-let users = {}; // id -> { ws, nick }
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
 
-function broadcastUserList() {
-    const userList = Object.values(users).map(u => u.nick);
-    const msg = JSON.stringify({ type: 'user-list', users: userList });
-    Object.values(users).forEach(u => u.ws.send(msg));
-}
+app.use(cors());
+app.get("/", (req, res) => res.send("Server çalışıyor"));
 
-wss.on('connection', (ws) => {
-    const userId = uuidv4();
-    users[userId] = { ws, nick: 'Anon' };
+let users = {};
 
-    // ID gönder
-    ws.send(JSON.stringify({ type: 'id', id: userId }));
+io.on("connection", (socket) => {
+  console.log("Yeni bağlantı:", socket.id);
 
-    ws.on('message', (msg) => {
-        let data;
-        try { data = JSON.parse(msg); } catch(e){ return; }
+  socket.on("join", (nick) => {
+    users[socket.id] = nick;
+    io.emit("userlist", Object.values(users));
+    console.log(nick, "katıldı");
+  });
 
-        if(data.type === 'join') {
-            users[userId].nick = data.nick || 'Anon';
-            broadcastUserList();
-        } else if(data.type === 'offer' || data.type === 'answer' || data.type === 'ice-candidate') {
-            const to = data.to;
-            if(users[to]) {
-                users[to].ws.send(JSON.stringify({
-                    ...data,
-                    from: userId
-                }));
-            }
-        }
-    });
+  socket.on("chat", (data) => {
+    io.emit("chat", data);
+  });
 
-    ws.on('close', () => {
-        delete users[userId];
-        broadcastUserList();
-    });
+  // WebRTC sinyalleme
+  socket.on("ready", () => {
+    for (let id in users) {
+      if (id !== socket.id) {
+        socket.to(id).emit("offer", { from: socket.id, offer: null });
+      }
+    }
+  });
+
+  socket.on("offer", (data) => {
+    socket.to(data.to).emit("offer", { from: socket.id, offer: data.offer });
+  });
+
+  socket.on("answer", (data) => {
+    socket.to(data.to).emit("answer", { from: socket.id, answer: data.answer });
+  });
+
+  socket.on("candidate", (data) => {
+    socket.to(data.to).emit("candidate", { from: socket.id, candidate: data.candidate });
+  });
+
+  socket.on("disconnect", () => {
+    delete users[socket.id];
+    io.emit("userlist", Object.values(users));
+  });
 });
 
-console.log("WebSocket server çalışıyor: ws://localhost:8080");
+server.listen(10000, () => console.log("Server 10000 portunda çalışıyor"));
